ccd643c9d40a96161f41aee529de257f061ff515
dichenli <dichenli@sas.upenn.edu>
2014-11-07 16:04:03 -0500

    first commit

---
+++ src/cit591_hw10/Layout.java
@@ -0,0 +1,80 @@
+package cit591_hw10;
+
+public class Layout {
+	public Layout(int[][] array){
+		
+	}
+	public Layout(int[] array){
+		
+	}
+	
+	public Layout(int length){
+		
+	}
+	
+	public Layout reverse() {
+		return null;
+	}
+	public Layout rotateRight() {
+		return null;		
+	}
+
+	public Layout rotateLeft() {
+		return null;		
+	}
+	public Layout transpose() {
+		return null;		
+	}
+	public Layout ravel(int n) {
+		return null;		
+	}
+	public Layout unravel() {
+		return null;		
+	}
+	public Layout reshape(int n) {
+		return null;		
+	}
+	public Layout join(Layout layout) {
+		return null;		
+	}
+	public Layout stack(Layout layout) {
+		return null;		
+	}
+	public int rowCount() {
+		return null;		
+	}
+	public int columnCount(){
+		return null;		
+	}
+	public Layout rows(int firstRow, int lastRow) {
+		return null;	
+	}
+	public Layout columns(int firstColumn, int lastColumn) {
+		return null;	
+	}
+	public Layout slice(int firstRow, int lastRow, int firstColumn, int lastColumn) {
+		return null;	
+	}
+	public Layout replace(Layout layout, int row, int column) {
+		return null;
+	}
+	@Override
+	public boolean equals(Object o){
+		return null;
+	}
+
+	@Override
+	public int hashCode(){
+		return null;
+	}
+	public int[] toArray1D() {
+		return null;
+	}
+	public int[][] toArray2D() {
+		return null;
+	}
+	public int at(int row, int column) {
+		return null;
+	}
+
+}
---
+++ src/cit591_hw10/LayoutTest.java
@@ -0,0 +1,129 @@
+package cit591_hw10;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LayoutTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLayoutIntArrayArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLayoutIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLayoutInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReverse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRotateRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRotateLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTranspose() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRavel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnravel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReshape() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJoin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStack() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRowCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testColumnCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRows() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testColumns() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSlice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReplace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToArray1D() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToArray2D() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAt() {
+		fail("Not yet implemented");
+	}
+
+}
